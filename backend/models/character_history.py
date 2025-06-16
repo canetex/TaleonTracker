8e6a83842480b0852c3495ff99e2cdb0ab3b3699
TheCrusty <canetex@gmail.com>
2025-06-16 19:06:12 -0300

    fix: corrige tipo do campo experience e adiciona campo deaths no modelo CharacterHistory

--- backend/models/character_history.py
+++ backend/models/character_history.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -8,7 +8,8 @@ class CharacterHistory(Base):
     id = Column(Integer, primary_key=True, index=True)
     character_id = Column(Integer, ForeignKey("characters.id"))
     level = Column(Integer)
-    experience = Column(Integer)
+    experience = Column(Float)
+    deaths = Column(Integer, default=0)
     timestamp = Column(DateTime)
     
     character = relationship("Character", back_populates="history") 
