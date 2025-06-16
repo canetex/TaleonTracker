db1be197547a3534b92be70a55dbd5ec1acabb91
TheCrusty <canetex@gmail.com>
2025-06-16 01:22:12 -0300

    fix: configura serviço para escutar em todas as interfaces

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8001/api';
+const baseURL = process.env.REACT_APP_API_URL || 'http://10.0.3.100:8001/api';
 
 export const api = axios.create({
   baseURL,
